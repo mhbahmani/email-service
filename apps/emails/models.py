d3deeab47d23e12834d879fe0fa4fb14be21ab09
mhbahmani <mhbahmani79@gmail.com>
2021-01-11 12:37:05 +0330

    add Email model add some documentation

--- apps/emails/models.py
+++ apps/emails/models.py
@@ -5,6 +5,11 @@ import re
 
 
 class EmailTemplate(models.Model):
+    '''
+        Emails have a HTML and Markdown template.
+        You can create a template and use it for different emails.
+        Templates have some HTML variables that admin can fill them in Emails section of admin panel.
+    '''
     FIELDS_REGEX = '{{\s(\w+)\s}}'
     KEYWORDS_REGEX = '{{\s(\w+\.\w+)\s}}'
 
@@ -61,3 +66,39 @@ class EmailTemplateField(models.Model):
     field_name = models.TextField()
     field_value = models.TextField(blank=True, null=True)
     template = models.ForeignKey(EmailTemplate, on_delete=models.CASCADE)
+
+
+class EmailRecipient(models.Model):
+    '''
+        This model specifies email recipients.
+        Each Email has a ManyToMany relation with this model.
+    '''
+    email_address = models.EmailField()
+
+    '''
+        Make a connection between emailRecipient and user in project.
+        Should be replaced with your User model.
+    '''
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Email(models.Model):
+    '''
+        When you want to set a value for email fields, you should put values in this format:
+        value1 $$ value2 $$ value3
+        OR:
+        values1
+        $$ value2 $$ value3
+        Actually just "$$" is important!
+    '''
+    TEMPLATE_FIELDS_SPLITTER = '\s?\n?\$\$\s?\n?'
+
+    template = models.ForeignKey(to=EmailTemplate, on_delete=models.CASCADE)
+    subject = models.CharField(blank=True, null=True, max_length=100)
+    values_of_fields = models.TextField(blank=False, null=False)
+    html_context = models.TextField()
+    text_context = models.TextField()
+    recipients = models.ManyToManyField(to=EmailRecipient)
+
+    def __str__(self):
+        return self.subject
