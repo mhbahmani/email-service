39edc1ec7c9225e8b871473e48d9159413a22f1e
mhbahmani <mhbahmani79@gmail.com>
2021-01-11 13:08:13 +0330

    add Profile to accounts app

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile',
+                                on_delete=models.CASCADE)
+
+    # Personal info
+    birth_date = models.DateField(null=True)
+    phone_number = models.CharField(max_length=32, null=True)
+
+    # Academic info
+    university = models.CharField(max_length=256)
