d43949f6ae7ae633b5c2a874cacc757637978c13
mhbahmani <mhbahmani79@gmail.com>
2021-02-11 17:16:21 +0330

    remove a print

--- emailService/settings.py
+++ emailService/settings.py
@@ -128,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-print(PRODUCTION)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
